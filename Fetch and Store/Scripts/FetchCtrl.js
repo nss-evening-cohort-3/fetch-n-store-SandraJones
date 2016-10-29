6beb34eda2f9a25392b02fdfc08f6880feac6183
Sandy Jones <sandynashv@gmail.com>
2016-10-29 13:40:42 -0500

    response time

--- Fetch and Store/Scripts/FetchCtrl.js
+++ Fetch and Store/Scripts/FetchCtrl.js
@@ -3,21 +3,22 @@
    
     $scope.address = "";
     $scope.status = "";
-    $scope.httpMethod = "";
-    $scope.responseTime = "";
+    $scope.httpMethod = document.getElementById("getButton");
+    $scope.elapsed;
 
     $scope.URLmethod = function () {
-
+        var begin = Date.now();
         console.log("fetchcontroller is here");
         console.log($scope.address);
         $http.get($scope.address)
             .then(function success(response) {
-                console.log(response);
-                $scope.status = response.status;
+                var end = Date.now();              
+                $scope.status = response.status;          
+                $scope.elapsed = end - begin;
             }, function error(response) {
                 $scope.title = "Oops... something went wrong";
                 $scope.working = false;
-            });
+                
+            });        
     };
 });
-
